4ef941f7e087506670c5d3677621b1e797203693
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-10 22:49:54 +0600

    service created for new book

--- src/app/modules/book/book.services.ts
+++ src/app/modules/book/book.services.ts
@@ -1,11 +1,26 @@
 import { Book } from "@prisma/client";
+import { prisma } from "../../constants/prisma_constructor";
 
-async function createBookIntoDb(payload: Partial<Book>) {
+async function createBookIntoDb(payload: Book) {
 
     try {
-        const result = await 
-    } catch (error) {
+        const result = await prisma.book.create({
+            data: payload
+        });
 
+        return {
+            success: true,
+            status: 201,
+            message: 'Book Created Successfully',
+            data: result
+        }
+
+    } catch (error) {
+        return {
+            success: false,
+            status: 400,
+            message: 'Failed to create new book'
+        }
     }
 };
 
