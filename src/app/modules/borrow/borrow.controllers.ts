d510bc43ccd154a078916be69d4e38de08365563
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-11 09:37:26 +0600

    brrow controller created for creating new borrow record into db

---
+++ src/app/modules/borrow/borrow.controllers.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { BorrowServices } from "./borrow.services";
+import { Borrow } from "@prisma/client";
+
+async function createNewBorrow(req: Request, res: Response) {
+
+    try {
+        const result = await BorrowServices.createNewBorrowIntoDb(req.body as Borrow);
+        res.status(result.status).json(result)
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            status: 500,
+            message: "Internal server error"
+        });
+    }
+};
+
+export const BorrowControllers = {
+    createNewBorrow
+}
